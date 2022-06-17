9ba4a0a06fa73df9a107df060027373050ff5860
Andy Garcia| <andy_gar@yahoo.com>
2022-06-17 19:24:32 -0300

    projeto portifolio

---
+++ kenziejavascript/index.js
@@ -0,0 +1,12 @@
+// exercicio 5 // 
+produto = 'maça';
+quantidadeProduto = 6;
+quantidadeProdutoPromocao = 5 - quantidadeProduto; 
+
+if( produto == 'maça' && quantidadeProduto >= 5)
+{
+    console.log('Você recebeu uma promoção, o valor do produto será R$ 5.00')
+    }else {
+    console.log(' Se você adicionar mais ' + quantidadeProdutoPromocao  + ' maças, o valor sairá por R$ 5.00.')
+}
+
